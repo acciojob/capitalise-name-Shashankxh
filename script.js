1c54b2d38ecd437c8db84e1969730e36737e282a
Shashank <139771571+Shashankxh@users.noreply.github.com>
2024-10-03 10:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function capitalizeName() {
+            // Get the input field by its id 'fname'
+            let inputField = document.getElementById('fname');
+            
+            // Convert the input value to uppercase
+            inputField.value = inputField.value.toUpperCase();
+        }
